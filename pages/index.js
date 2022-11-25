5a59fe6f89c33da007d30658d7e1e525d73756e5
atharv_arolkar <atharv_arolkar@persistent.com>
2022-11-25 09:31:15 +0530

    changed the code

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,15 @@ export default function Home() {
 
   useEffect(() => {
     window.OneSignal = window.OneSignal || [];
+    
   OneSignal.push(function() {
     OneSignal.init({
-      appId: "5ceb58cc-97ee-4d13-a80f-7e5a68c19836",
-      safari_web_id: "web.onesignal.auto.246fdfe2-a404-4d40-aa8a-d2b211d431d5",
+      appId: "b5b2ecf6-847a-427d-bfca-bad2804be000",
+      safari_web_id: "web.onesignal.auto.092506a7-b452-4a06-a822-c1d343884087",
       notifyButton: {
         enable: true,
       },
-      subdomainName: "push-not",
+      // subdomainName: "push-not",
     });
   },[]);
 
