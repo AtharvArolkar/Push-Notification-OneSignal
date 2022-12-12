d39f231678cf71ef552ddff62de9843bb41107d0
Tanad Bandodkar <56749596+TanadBandodkar@users.noreply.github.com>
2022-12-12 11:43:12 +0530

    Update index.js
    Added A2HS feature for chrome

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,63 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import Image from 'next/image'
-import { useEffect } from 'react'
+import { useEffect} from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
-
   useEffect(() => {
-  //   window.OneSignal = window.OneSignal || [];
-    
-  // OneSignal.push(function() {
-  //   OneSignal.init({
-  //     appId: "b5b2ecf6-847a-427d-bfca-bad2804be000",
-  //     safari_web_id: "web.onesignal.auto.092506a7-b452-4a06-a822-c1d343884087",
-  //     notifyButton: {
-  //       enable: true,
-  //     },
-  //     // subdomainName: "push-not",
-  //   });
-  // },[]);
+    //   window.OneSignal = window.OneSignal || [];
 
-  //   return () => {
-  //       window.OneSignal = undefined;
-  //   };
-  window.OneSignal = window.OneSignal || [];
-  OneSignal.push(function() {
-    OneSignal.init({
-      appId: "78417f63-3be8-4dff-829a-35853d9c9801",
+    // OneSignal.push(function() {
+    //   OneSignal.init({
+    //     appId: "b5b2ecf6-847a-427d-bfca-bad2804be000",
+    //     safari_web_id: "web.onesignal.auto.092506a7-b452-4a06-a822-c1d343884087",
+    //     notifyButton: {
+    //       enable: true,
+    //     },
+    //     // subdomainName: "push-not",
+    //   });
+    // },[]);
+
+    //   return () => {
+    //       window.OneSignal = undefined;
+    //   };
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+      OneSignal.init({
+        appId: "78417f63-3be8-4dff-829a-35853d9c9801",
+      });
     });
-  });
-}, [])
-
+  }, [])
 
   return (
     <div className={styles.container}>
       <Head>
-  <title>OneSignal + Next.js</title>
-  <meta
-    name="description"
-    content="Integrating OneSignal with a Next.js app."
-  />
-  <link rel="icon" href="/favicon.ico" />
-  <script
-    src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
-    async=""
-  ></script>
+        <title>OneSignal + Next.js</title>
+        <meta
+          name="description"
+          content="Integrating OneSignal with a Next.js app."
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"></link>
+        <script
+          src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+          async=""
+        ></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <script src="/Push-Notification-OneSignal-main/public/js/index.js"></script>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div>
+          <button className={`addbutton btn waves-effect waves-light`} >Add to home screen
+            {/* <i className="material-icons right">send</i> */}
+          </button>
+          <h1 className={styles.title}>
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </h1>
+        </div>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -101,19 +108,49 @@ export default function Home() {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
 
-
-
-
-
-          
-            
-
-
-
-
-
         </a>
       </footer>
+      <Script>
+        {`
+            let deferredPrompt;
+            const addBtn = document.querySelector(".addbutton");
+            console.log(addBtn);
+            addBtn.style.display = "none";
+            
+            window.addEventListener("beforeinstallprompt", (e) => {
+                // Prevent Chrome 67 and earlier from automatically showing the prompt
+                e.preventDefault();
+                // Stash the event so it can be triggered later.
+                deferredPrompt = e;
+                // Update UI to notify the user they can add to home screen
+                addBtn.style.display = "block";
+                listenToUserAction();
+            });
+            
+            // listen to install button clic
+            function listenToUserAction() {
+                addBtn.addEventListener("click", presentAddToHome);
+            }
+            
+            // addBtn.addEventListener("click", (e) => {
+            
+            function presentAddToHome() {
+                // hide our user interface that shows our A2HS button
+                addBtn.style.display = "none";
+                // Show the prompt
+                deferredPrompt.prompt();
+                // Wait for the user to respond to the prompt
+                deferredPrompt.userChoice.then((choiceResult) => {
+                    if (choiceResult.outcome === "accepted") {
+                        console.log("User accepted the A2HS prompt");
+                    } else {
+                        console.log("User dismissed the A2HS prompt");
+                    }
+                    deferredPrompt = null;
+                });
+            };
+        `}
+      </Script>
     </div>
   )
 }
